d4330e0d845ebb15715cbe6436b0eb1f3a1b6a26
Shane Wells <shane130798@live.co.uk>
2018-01-24 15:39:57 +0000

    added more functionality to solution

--- surgery/surgery/surgery.cpp
+++ surgery/surgery/surgery.cpp
@@ -6,41 +6,61 @@
 #include <iostream>
 #include <array>
 #include <string>
+#include <STDLIB.H>
 
 using namespace std;
 
 class login {
+public:
 	void GetDetails(string sUsernamepass, string sPasswordpass);
-
-private:
+	bool bCheckLogin();
+protected:
 	string sUsername = "";
 	string sPassword = "";
+	bool bVerify = false;
 };
 
-void GetDetails(string sUsernamepass, string sPasswordpass)
+void login::GetDetails(string sUsernamepass, string sPasswordpass)
 {
 	string sUsername = sUsernamepass;
 	string sPassword = sPasswordpass;
+}
 
-
+bool login::bCheckLogin()
+{
+	bool bVerify = false;
+	if (sUsername == "Admin" && sPassword == "Password")
+	{
+		cout << bVerify;
+		bVerify = true;
+	}
+	else
+	{
+		cout << "\n You've entered the incorrect Username or Password";
+	}
+ return bVerify = true;
 }
 
 
 int main()
 {
 	login ologin;
-
 	string sUsername;
 	string sPassword;
+	bool bVerify;
 
 	cout << "Welcome to the program";
-	cout << "\n\n Please enter your Username: ";
-	cin >> sUsername;
-	cout << "\n please enter your Password: ";
-	cin >> sPassword;
 
-	oLogin:GetDetails(sUsername, sPassword);
-
+	//for (int iCount; iCount < 3; iCount++)
+	//{
+		cout << "\n\n Please enter your Username: ";
+		cin >> sUsername;
+		cout << "\n please enter your Password: ";
+		cin >> sPassword;
+		ologin.GetDetails(sUsername, sPassword);
+		ologin.bCheckLogin();
+	//}
+	_getch();
     return 0;
 }
 
