8e4ca39cc611bfb0622a0e11eb0aee2b5f4778e9
Shane Wells <shane130798@live.co.uk>
2018-01-24 13:07:55 +0000

    adding core code to solution.

---
+++ surgery/surgery/surgery.cpp
@@ -0,0 +1,22 @@
+// surgery.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <conio.h>
+#include <iostream>
+#include <array>
+#include <string>
+
+using namespace std;
+
+class user {
+	
+};
+
+
+
+int main()
+{
+    return 0;
+}
+
