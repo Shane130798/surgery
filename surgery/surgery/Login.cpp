b8f47fcc9f1cc687a7d2d9ff747a76a1831dc6b8
Shane Wells <shane130798@live.co.uk>
2018-03-07 09:21:18 +0000

    changes

--- surgery/surgery/Login.cpp
+++ surgery/surgery/Login.cpp
@@ -13,6 +13,142 @@
 
 using namespace std;
 
+void SurgeryMenu()
+{
+	bool bExit = false;
+	int iChoice = 0;
+	cout << "/nSurgeryMenu";
+	cout << "/n/n Select from These options:";
+	cout << "1. Option\n ";
+	cout << "2. Option\n ";
+	cout << "3. Option\n ";
+	cout << "4. Option\n ";
+	cin >> iChoice;
+
+	do {
+		switch (iChoice) {
+		default:
+			cout << "Please Select from the options above.\n";
+		case 1:
+			cout << "You have selected: ";
+			break;
+		case 2:
+			cout << "You have selected: ";
+			break;
+		case 3:
+			cout << "You have selected: ";
+			break;
+		case 4:
+			cout << "You have selected: ";
+			break;
+		}
+	} while (bExit = false);
+}
+
+void DoctorsMenu()
+{
+	bool bExit = false;
+	int iChoice = 0;
+	cout << "/nSurgeryMenu";
+	cout << "/n/n Select from These options:";
+	cout << "1. Option\n ";
+	cout << "2. Option\n ";
+	cout << "3. Option\n ";
+	cout << "4. Option\n ";
+	cin >> iChoice;
+
+	do {
+		switch (iChoice) {
+		default:
+			cout << "Please Select from the options above.";
+		case 1:
+			cout << "You have selected: ";
+			break;
+		case 2:
+			cout << "You have selected: ";
+			break;
+		case 3:
+			cout << "You have selected: ";
+			break;
+		case 4:
+			cout << "You have selected: ";
+			break;
+		}
+	} while (bExit = false);
+}
+
+void BookingMenu()
+{
+
+}
+
+void BookingOverview()
+{
+	bool bExit = false;
+	int iChoice = 0;
+	cout << "/nSurgeryMenu";
+	cout << "/n/n Select from These options:";
+	cout << "1. Option\n ";
+	cout << "2. Option\n ";
+	cout << "3. Option\n ";
+	cout << "4. Option\n ";
+	cin >> iChoice;
+
+	do {
+		switch (iChoice) {
+		default:
+			cout << "Please Select from the options above.";
+		case 1:
+			cout << "You have selected: ";
+			break;
+		case 2:
+			cout << "You have selected: ";
+			break;
+		case 3:
+			cout << "You have selected: ";
+			break;
+		case 4:
+			cout << "You have selected: ";
+			break;
+		}
+	} while (bExit = false);
+}
+
+void MainMenu()
+{
+	bool bExit = false;
+
+	do {
+		int iChoice = 0;
+		cout << "\n\n Please select from one of the below. \n";
+		cout << "1.Surgery Information \n"; /*Viewing Doctors, etc.*/
+		cout << "2.Doctors Info \n"; /*Doctor and Speciality, Appointtments*/
+		cout << "3.Booking System \n";/*Booking New Patient in*/
+		cout << "4.Booking Overview \n";/*Appointments booked into Surgery*/
+		cout << "5.Exit Program \n"; /*really? You actually need me to explain this.. Wot do u fink it dos, eh?*/
+		cin >> iChoice;
+		switch (iChoice)
+		{
+		default:
+			break;
+		case 1:
+			SurgeryMenu();
+			break;
+		case 2:
+			DoctorsMenu();
+			break;
+		case 3:
+			BookingMenu();
+			break;
+		case 4:
+			BookingOverview();
+			break;
+		case 5:
+			_getch();
+			EXIT_FAILURE;
+		}
+	} while (bExit = false);
+}
 
 int main()
 {
